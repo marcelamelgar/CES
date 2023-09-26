c916282ebfd351b67d8e22630f3d57a0d68f13eb
Marce Melgar <marcelamelgar@office.ufm.edu>
2023-09-26 09:34:56 -0600

    ces-project

---
+++ src/app/dashboard-control/dashboard-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-control',
+  templateUrl: './dashboard-control.component.html',
+  styleUrls: ['./dashboard-control.component.css']
+})
+export class DashboardControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/assets/js/my-script.js
@@ -0,0 +1,3 @@
+import 'jquery/dist/jquery.slim.min';
+import 'popper.js/dist/umd/popper.min';
+import 'bootstrap/dist/js/bootstrap.min';
